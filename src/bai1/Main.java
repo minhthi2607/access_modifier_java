ef25b870a322c49d04fb8ea113d77042db51a3f5
Phan Minh Thi <thi@corsivalab.space>
2025-12-15 08:45:09 +0700

    circle

---
+++ src/bai1/Main.java
@@ -0,0 +1,9 @@
+package bai1;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle obj = new Circle(5.5);
+        System.out.println(obj.getArea());
+        System.out.println(obj.getRadius());
+    }
+}
