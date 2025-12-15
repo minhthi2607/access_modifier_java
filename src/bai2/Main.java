415eed51f2606683e57d1491c4e9e531195a71c6
Phan Minh Thi <thi@corsivalab.space>
2025-12-15 08:48:47 +0700

    student

---
+++ src/bai2/Main.java
@@ -0,0 +1,11 @@
+package bai2;
+
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setName("Rahul");
+        s1.setClasses("CO3");
+        System.out.println(s1.getName());
+        System.out.println(s1.getClasses());
+    }
+}
